152bba7bffbf57470669438643c9383fdf8bc470
Oleg.D <demchukoleg88@gmail.com>
2025-02-14 21:35:00 +0100

    Add base configuration for eslint, prettier and storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
-import eslintConfigPrettier from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -11,8 +10,16 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
-	eslintConfigPrettier,
+	...compat.config({
+		extends: [
+			'next/core-web-vitals',
+			'next/typescript',
+			'plugin:prettier/recommended',
+		],
+		rules: {
+			'react/jsx-closing-bracket-location': 'error',
+		},
+	}),
 ]
 
 export default eslintConfig
