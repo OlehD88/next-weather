1af0cd7e6fa0f2e29c6b667285d33bfaa71d4733
Oleg.D <demchukoleg88@gmail.com>
2025-02-16 19:02:19 +0100

    Add some basic spinner for preloading

--- src/app/(home)/_sections/CurrentWeatherSection.tsx
+++ src/app/(home)/_sections/CurrentWeatherSection.tsx
@@ -6,6 +6,7 @@ import { CurrentWeather } from '@/components/CurrentWeather/CurrentWeather'
 import { useForecast } from '@/contexts/forecastContext'
 import { useLocation } from '@/contexts/locationContext'
 import { getLocationName } from '@/utils/location'
+import Image from 'next/image'
 
 export const CurrentWeatherSection = () => {
 	const {
@@ -25,20 +26,28 @@ export const CurrentWeatherSection = () => {
 	const locationName = getLocationName(locationData)
 	const currentWeather = currentWeatherConditions
 
-	if (!currentWeather) {
-		return 'Loading...'
-	}
-
 	return (
 		<section className="dark-section pt-14 pb-32 flex justify-center">
-			<CurrentWeather
-				unit={temperatureUnit}
-				location={locationName || ''}
-				temperature={currentWeather?.temperature?.[temperatureUnit]}
-				weatherInfo={currentWeather?.weatherInfo}
-				date={currentWeather?.date}
-				onUnitChange={setTemperatureUnit}
-			/>
+			{!currentWeather ? (
+				<div>
+					<Image
+						src="/spinner.svg"
+						alt="Loading"
+						width={160}
+						height={160}
+						className="animate-spin"
+					/>
+				</div>
+			) : (
+				<CurrentWeather
+					unit={temperatureUnit}
+					location={locationName || ''}
+					temperature={currentWeather?.temperature?.[temperatureUnit]}
+					weatherInfo={currentWeather?.weatherInfo}
+					date={currentWeather?.date}
+					onUnitChange={setTemperatureUnit}
+				/>
+			)}
 		</section>
 	)
 }
