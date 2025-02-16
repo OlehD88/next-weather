1662389f43c47f531f08e01427184d1508d4de3b
Oleg.D <demchukoleg88@gmail.com>
2025-02-16 15:39:43 +0100

    Add current conditions api endpoint

---
+++ src/app/api/currentConditionsHistory/[locationKey]/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+import axios, { AxiosError } from 'axios'
+
+export const GET = async (req: NextRequest, { params }: { params: Promise<{ locationKey: string }> }) => {
+	const locationKey = (await params).locationKey
+	const apikey = process.env.WEATHER_API_KEY
+	const baseApiUrl = process.env.WEATHER_BASE_API_URL
+	const apiUrl = `${baseApiUrl}/currentconditions/v1/${locationKey}/historical/24`
+
+	try {
+		const response = await axios.get(apiUrl, {
+			params: {
+				apikey,
+			},
+		})
+		return NextResponse.json(response.data, {
+			status: 200,
+		})
+	} catch (e) {
+		if (e instanceof AxiosError) {
+			return NextResponse.json({ error: e.message }, { status: e.status })
+		}
+	}
+}
